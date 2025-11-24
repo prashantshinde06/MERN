776a72add17341667205c419056e90df1866176e
Prashant Shinde <shindeprashant3472@gmail.com>
2025-11-24 22:31:30 +0530

    new changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserRegistration from "./components/userregistration/UserRegistration";
 import UserLogin from "./components/userlogin/UserLogin";
